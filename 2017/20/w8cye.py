0c51b0dd6c58a4345851300aa819643433f18a62
Josh Peterson <ccie7207@gmail.com>
2024-09-21 09:40:54 -0400

    day21 and update to day20

--- 2017/20/w8cye.py
+++ 2017/20/w8cye.py
@@ -13,24 +13,27 @@ class Vector:
     y: int
     z: int
 
-    def __post_init__(self):
+    def __post_init__(self) -> None:
         self.update_distance()
 
-    def __iadd__(self, other: Self):
+    def __iadd__(self, other: Self) -> Self:
         self.x += other.x
         self.y += other.y
         self.z += other.z
         self.update_distance()
         return self
 
-    def update_distance(self):
-        self.distance = abs(self.x) + abs(self.y) + abs(self.z)
+    def distance_to(self, other: Self) -> int:
+        return abs(self.x - other.x) + abs(self.y - other.y) + abs(self.z - other.z)
 
-    def __hash__(self) -> int:
-        return hash((self.x, self.y, self.z))
+    def update_distance(self) -> None:
+        self.distance = abs(self.x) + abs(self.y) + abs(self.z)
 
     def __repr__(self) -> str:
         return f"({self.x},{self.y},{self.z})"
+
+    def get_coords(self) -> tuple[int, int, int]:
+        return (self.x, self.y, self.z)
 
 
 @dataclass(order=True)
@@ -41,15 +44,20 @@ class Particle:
     input_string: str
     index: int
 
+    def __hash__(self) -> int:
+        return self.index
+
     def __post_init__(self):
         matches = re.findall(r"<([^>]+)>", self.input_string)
         vals = [int(val) for match in matches for val in match.split(",")]
         self.pos = Vector(*vals[:3])
         self.vel = Vector(*vals[3:6])
         self.acc = Vector(*vals[6:])
+        self.previous_pos = Vector(*vals[:3])
 
     def advance(self):
         self.vel += self.acc
+        self.previous_pos = Vector(self.pos.x, self.pos.y, self.pos.z)
         self.pos += self.vel
 
     def __repr__(self) -> str:
@@ -63,12 +71,13 @@ def part1(input_data: str) -> int:
 
 def part2(input_data: str) -> int:
     data = [Particle(line, i) for i, line in enumerate(input_data.splitlines())]
-    for i in range(100):
-        seen: dict[Vector, int] = defaultdict(int)
+    for i in range(50):
+        seen: dict[tuple[int, int, int], int] = defaultdict(int)
         for particle in data:
             particle.advance()
-            seen[particle.pos] += 1
-        data = [particle for particle in data if seen[particle.pos] == 1]
+            seen[particle.pos.get_coords()] += 1
+        data = [particle for particle in data if seen[particle.pos.get_coords()] == 1]
+
     return len(data)
 
 
